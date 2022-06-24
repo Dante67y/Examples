4059cf21e70fdab731d4473ed4e8c844bc9f73b1
Dante67yt <dante67yt@gmail.com>
2022-06-24 18:55:14 +0300

    add Example 005 If Else

---
+++ Ex005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Сима")
+{
+    Console.WriteLine("Ура, это же СИМА!");
+}
+else
+{
+    Console.Write("Првиет, ");
+    Console.WriteLine(username);
+}
