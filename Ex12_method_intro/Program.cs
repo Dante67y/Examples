b6c59ab100084cf22b18cda2141c434c1d75a03c
Dante67yt <dante67yt@gmail.com>
2022-06-26 14:50:44 +0300

    change method

--- Ex12_method_intro/Program.cs
+++ Ex12_method_intro/Program.cs
@@ -11,7 +11,7 @@ int b1 = 2334;
 int c1 = 12;
 int a2 = 39;
 int b2 = 6545;
-int c2 = 743;
+int c2 = 7433;
 int a3 = 13;
 int b3 = 24;
 int c3 = 87;
@@ -26,9 +26,11 @@ int c3 = 87;
 //if (b3 > max) max = b3;
 //if (c3 > max) max = c3;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 
 Console.WriteLine(max);
