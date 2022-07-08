b739da6437f147fb03f530ff4faf50541580067b
Dante67yt <dante67yt@gmail.com>
2022-07-09 00:14:00 +0300

    add Ex018 sycles

---
+++ Ex018_sycles/Program.cs
@@ -0,0 +1,70 @@
+﻿// string Method4(int count, string s)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+//     while (i < count)
+//     {
+//         result = result + s;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "zZz...");
+// Console.WriteLine(res);
+
+// string Method4(int count, string s)
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result = result + s;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "zZz...");
+// Console.WriteLine(res);
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} * {j} = {i * j}");
+//     }
+//     Console.WriteLine();
+// }
+
+//===== Работа с текстом
+// Дан тест. В текстенужно все пробелы заменить черточками,
+// маленькие буквы "к" заменить большими "К"Б
+// а большие "С" заменить маленькими "с".
+
+string text = "- Я думаю, - сказалкнязь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             012345
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int lenght = text.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
+
