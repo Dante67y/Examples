cb28adcb9b74150d1999f44af7fe6d28564d1328
Dante67yt <dante67yt@gmail.com>
2022-07-09 00:13:38 +0300

    add Ex017 functions adn changes in Ex016

---
+++ Ex016_seminar3_1/Program.cs
@@ -0,0 +1,52 @@
+﻿// int x, y;
+// x = Convert.ToInt32(Console.ReadLine());
+// y = Convert.ToInt32(Console.ReadLine());
+// if (x > 0 && y > 0) Console.WriteLine("1 четверть");
+// if (x < 0 && y > 0) Console.WriteLine("2 четверть");
+// if (x > 0 && y < 0) Console.WriteLine("3 четверть");
+// if (x < 0 && y < 0) Console.WriteLine("4 четверть");
+
+// int n;
+// n = Convert.ToInt32(Console.ReadLine());
+// if (n > 0 && n < 5){
+//     if (n == 1){
+//         Console.WriteLine("x = (0, +~)");
+//         Console.WriteLine("y = (0, +~)");
+//     }
+//     if (n == 2){
+//         Console.WriteLine("x = (-~, 0)");
+//         Console.WriteLine("y = (0, +~)");
+//     }
+//     if (n == 3){
+//         Console.WriteLine("x = (0, +~)");
+//         Console.WriteLine("y = (-~, 0)");
+//     }
+//     if (n == 4){
+//         Console.WriteLine("x = (-~, 0)");
+//         Console.WriteLine("y = (-~, 0)");
+//     }
+// }
+// else Console.WriteLine("Такой четверти НЕТ!");
+
+// double x, y, x1, y1, len;
+// x = Convert.ToDouble(Console.ReadLine());
+// y = Convert.ToDouble(Console.ReadLine());
+// x1 = Convert.ToDouble(Console.ReadLine());
+// y1 = Convert.ToDouble(Console.ReadLine());
+// len = Math.Sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1));
+// Console.WriteLine(len);
+
+// int n;
+// n = Convert.ToInt32(Console.ReadLine());
+// for (int i = 1; i <= n; i++)
+// {
+//     Console.WriteLine(i * i);
+// }
+
+int n, i = 1;
+n = Convert.ToInt32(Console.ReadLine());
+while (i <= n)
+{
+    Console.WriteLine(i * i);
+    i++;
+}
---
+++ Ex017_functions/Program.cs
@@ -0,0 +1,52 @@
+﻿// Группы методов:
+
+// Вид 1 - ничего не принимают и ничего не возвращают
+// void Method1()
+// {
+//    Console.WriteLine("Автор...");
+// }
+// Method1(); (верно)
+// Method1; (неверно)
+
+// Вид 2 - что-то принимают и ничего не возвращают
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// Method2(msg: "Текст сообщения");
+
+// void Method2.1(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+// {
+//     Console.WwriteLine(msg);
+//     i++;
+// }
+//     Console.WriteLine(msg);
+// }
+// Method2(msg: "Текст сообщения", count: 4);
+// Method2(count: 4, msg: "Текст сообщения");
+
+// Вид 3 - ничего не принимают, но что-то возвращают
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+// int year = Method3;
+// Console.WriteLine(year);
+
+// Вид 4 - что-то принимают и что-то возвращают
+// string Method4(int count, string s)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+//     while (i < count)
+//     {
+//         result = result + s;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "zZz...");
+// Console.WriteLine(res);
