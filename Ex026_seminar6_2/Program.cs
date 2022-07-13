7f479e6b5ae1deed2c86c4d0dfc30298f8c01f80
Dante67yt <dante67yt@gmail.com>
2022-07-13 14:53:24 +0300

    add second block at seminar 6 some chabges

--- Ex026_seminar6_2/Program.cs
+++ Ex026_seminar6_2/Program.cs
@@ -1,6 +1,7 @@
 ﻿// // Напишите программу, которая будет преобразовывать десятичное число в двоичное
 
 // // Перевод числа в двоичную систему исчисления(0=0, 1=1, 2=10...10=1010...)
+
 // int count = 0, x, n = Convert.ToInt32(Console.ReadLine());
 // // Добавляем динамический массив через функцию List (нужно для разворота результата, потому что без такого
 // // массива терминал выдаст 0101)
@@ -20,23 +21,24 @@
 
 // Напишите программу, которая будет выводить первые "N" чисел Фибоначчи
 
-// мой вариант (скопировал с лекции и отредактировал)
-double Fibonacci(int n)
-{
-    if (n == 1 || n == 2) return 1;
-    return Fibonacci(n - 1) + Fibonacci(n - 2);
-}
-int n;
-Console.Write("Введите число: ");
-n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Высчитываем число Фибоначчи заданного числа:");
-for (int i = 1; i <= (n + 1); i++)
-{
-    Console.Write($"f({i - 1}) = {Fibonacci(i)}");
-    Console.Write("; ");
-}
+// // Мой вариант (скопировал с лекции и отредактировал)
 
-// // Вариант преподавателя
+// double Fibonacci(int n)
+// {
+//     if (n == 1 || n == 2) return 1;
+//     return Fibonacci(n - 1) + Fibonacci(n - 2);
+// }
+// int n;
+// Console.Write("Введите число: ");
+// n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Высчитываем число Фибоначчи заданного числа:");
+// for (int i = 1; i <= (n + 1); i++)
+// {
+//     Console.Write($"f({i - 1}) = {Fibonacci(i)}");
+//     Console.Write("; ");
+// }
+
+// // Вариант преподавателя (Денис)
 // int a0 = 0, a1 = 1, a2;
 // int n;
 // Console.Write("Введите число: ");
@@ -50,3 +52,14 @@ for (int i = 1; i <= (n + 1); i++)
 //     Console.Write(a2);
 //     Console.Write(" ");
 // }
+
+// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования
+
+int[] arr1 = new int[] {1, 2, 56, 0 -10};
+int[] arr2 = new int[arr1.Length];
+int x;
+for (int i = 0; i < arr2.Length; i++)
+{
+    x = arr1[i];
+    arr2[i] = x;
+}
