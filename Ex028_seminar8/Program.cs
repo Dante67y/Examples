c742d005ac9bb7d314a1be3c8cee814d73488ec1
Dante67yt <dante67yt@gmail.com>
2022-07-21 22:37:24 +0300

    add Ex028 seminar 8 part 1

---
+++ Ex028_seminar8/Program.cs
@@ -0,0 +1,57 @@
+﻿int[,] CreateMatr(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    return matrix;
+}
+
+//     // Меняем местами первую и последнюю строку массива
+// void PrintMatr(int[,] matrix)
+// {
+//     Console.WriteLine();
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         (matrix[0, j], matrix[matrix.GetLength(0) - 1, j]) 
+//         = (matrix[matrix.GetLength(0) - 1, j], matrix[0, j]);
+//     }
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write(matrix[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+void PrintMatr(int[,] matrix)
+{
+    Console.WriteLine();
+    if (matrix.GetLength(0) == matrix.GetLength(1))
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j > i)
+            {
+                (matrix[i, j], matrix[j, i]) = (matrix[j, i], matrix[i, j]);
+            }
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    else Console.WriteLine("Замена невозможна.");
+}
+Console.Write("Введите количество строк в массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+PrintMatr(CreateMatr(m, n));
